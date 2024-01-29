b1fd7932421e67df59200da63f93d321a9941ccf
obzhorka <karetnikovakseniya@gmail.com>
2024-01-29 21:29:56 +0100

    poprawiono błędy(serwer-client)

--- src/main/java/org/example/GoBoard.java
+++ src/main/java/org/example/GoBoard.java
@@ -11,11 +11,13 @@ public class GoBoard extends JPanel {
     private final int numberOfSquares = 9;
     private final int boardSize = gridSize * numberOfSquares;
     private char token;
+    private boolean hasBlackPassed = false;
+    private boolean hasWhitePassed = false;
 
     private final ArrayList<Intersection> intersections = new ArrayList<>();
 
-    public GoBoard() {
-        setPreferredSize(new Dimension(boardSize, boardSize));
+    public GoBoard(int boardSize) {
+        setPreferredSize(new Dimension(this.boardSize, this.boardSize));
         //reaguje na kliknięcia myszką na planszy
         addMouseListener(new IntersectionMouseListener());
     }
@@ -26,8 +28,8 @@ public class GoBoard extends JPanel {
     }
 
     // Dodaj nowe pola dla przesunięcia planszy
-    private final int offsetX = 50; // Przesunięcie w poziomie
-    private final int offsetY = 50; // Przesunięcie w pionie
+    final int offsetX = 50; // Przesunięcie w poziomie
+    final int offsetY = 50; // Przesunięcie w pionie
 //Dodaje nową intersekcję w określonym rzędzie i kolumnie na podstawie tokena
     public void setToken(int row, int column, char token) {
     intersections.add(new Intersection((column * gridSize) + 50, (row * gridSize) + 50, token == 'B' ? Color.BLACK : Color.WHITE));
@@ -56,6 +58,17 @@ public class GoBoard extends JPanel {
         }
     }
 
+    public int getGridSize() {
+        return 0;
+    }
+
+    public void setBlackPassed(boolean hasPassed) {
+        this.hasBlackPassed = hasPassed;
+    }
+
+    public void setWhitePassed(boolean hasPassed) {
+        this.hasWhitePassed = hasPassed;
+    }
 
     protected class IntersectionMouseListener extends MouseAdapter {
         @Override
