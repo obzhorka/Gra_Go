f985bdade683d645acc9b91a00845cdd9ac1ceea
gumkelsn <gullera.a22@gmail.com>
2024-01-30 01:36:59 +0100

    Zmaktualizowałam klase server. Usunęłam niepotrzebne części.

---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,36 @@
+package org.example;
+
+
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        int port = 1234; // Port, na którym serwer będzie nasłuchiwał
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Serwer uruchomiony na porcie " + port);
+
+        try {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                try {
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                    String request;
+                    while ((request = in.readLine()) != null) {
+                        System.out.println("Otrzymano: " + request);
+                        out.println("Echo: " + request);
+                        if ("koniec".equalsIgnoreCase(request)) {
+                            break;
+                        }
+                    }
+                } finally {
+                    clientSocket.close();
+                }
+            }
+        } finally {
+            serverSocket.close();
+        }
+    }
+}
